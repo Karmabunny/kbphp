96ab6346ee2c3c2863c2097187aa052f95d8c661
Gwilyn <gwilyn@karmabunny.com.au>
2020-10-21 13:05:36 +1030

    Added serialisation to collections.

--- src/Collection.php
+++ src/Collection.php
@@ -6,12 +6,15 @@
 
 namespace karmabunny\kb;
 
+use JsonSerializable;
+use Serializable;
+
 /**
  *
  *
  * @package karmabunny/kb
  */
-class Collection implements \ArrayAccess, \IteratorAggregate {
+class Collection implements \ArrayAccess, \IteratorAggregate, Serializable, JsonSerializable {
 
     function __construct(iterable $config = [])
     {
@@ -19,30 +22,56 @@ class Collection implements \ArrayAccess, \IteratorAggregate {
     }
 
 
+    /** @inheritdoc */
+    public function serialize(): string
+    {
+        return serialize($this->toArray());
+    }
+
+
+    /** @inheritdoc */
+    public function unserialize($serialized)
+    {
+        $this->update(unserialize($serialized));
+    }
+
+
+    /** @inheritdoc */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+
+    /** @inheritdoc */
     public function getIterator(): \Iterator
     {
         return new \ArrayIterator($this);
     }
 
 
+    /** @inheritdoc */
     public function offsetExists($offset)
     {
         return isset($this->$offset);
     }
 
 
+    /** @inheritdoc */
     public function offsetGet($offset)
     {
         return @$this->$offset ?: null;
     }
 
 
+    /** @inheritdoc */
     public function offsetSet($offset, $value)
     {
         $this->$offset = $value;
     }
 
 
+    /** @inheritdoc */
     public function offsetUnset($offset)
     {
         unset($this->$offset);
