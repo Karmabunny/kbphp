840468e348f361b32e9a62b2c5927b6ff4d96007
Gwilyn <gwilyn@karmabunny.com.au>
2020-11-16 11:42:28 +1030

    Added publish script.

---
+++ scripts/publish.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname $0)/.."
+
+UNPUSHED=$( git rev-list --count origin/master..master )
+if [ $UNPUSHED -gt 0 ]; then
+	echo "You have $UNPUSHED commit(s) on master which have not been pushed to origin"
+	echo "Aborting deployment"
+	exit 1
+fi
+
+echo "Pushing tags"
+git push --tags
+
+echo "Rebuild package repo"
+php -- - <<'PHP'
+<?php
+$out = file_get_contents('https://bsts.bunnysites.com/api/composer/run?name=packages&script=build');
+$out = json_decode($out, true);
+echo $out['output'], PHP_EOL;
+PHP
+
+echo "Done"
