293694debc38d1ebfe023d5505fbc4402d6c4438
gwillz <me@gwilyn.com>
2021-02-14 16:40:06 +1030

    Fix url tests.

--- tests/UrlHelperTest.php
+++ tests/UrlHelperTest.php
@@ -12,8 +12,9 @@ use PHPUnit\Framework\TestCase;
  */
 final class UrlHelperTest extends TestCase {
 
-    public function testBuildParts()
+    public function testWithBaseUrl()
     {
+        // Typical usage: the base url isn't cleaned, the path built and _is_ cleaned.
         $expected = 'https://example.com/api/path/to%3F/thing?param1=123&neat%5B0%5D=one&neat%5B1%5D=two';
         $actual = Url::build('https://example.com/api/', [
             '/path', '/to?/thing',
@@ -25,10 +26,34 @@ final class UrlHelperTest extends TestCase {
     }
 
 
-    public function testBuildDumb()
+    public function testDumbPaths()
     {
-        $expected = 'https://okay.com/its?all=good&hah[0]=123';
-        $actual = Url::build('https://okay.com', 'its?all=good&hah[0]=123');
+        // The 'dumb' version will trash the queries.
+        // But the base url and path should be fine.
+        $expected = 'https://okay.com/it/is/%3Fall%3Dgood%26hah%5B0%5D%3D123';
+        $actual = Url::build('https://okay.com', '///it/is', '?all=good&hah[0]=123');
+
+        $this->assertEquals($expected, $actual);
+
+        // The correct way would be to decode it first.
+        // Something like this.
+        $expected = 'https://okay.com/it/is?all=good&hah%5B0%5D=123';
+        [$path, $query] = explode('?', '///it/is?all=good&hah[0]=123');
+        $actual = Url::build('https://okay.com', $path, Url::decode($query));
+
+        $this->assertEquals($expected, $actual);
+    }
+
+
+    public function testBuildPath()
+    {
+        // A single path argument builds a path as expected and cleans everything.
+        $expected = '/it/is?all=good&hah%5B0%5D=123';
+        $actual = Url::build([
+            '//it//', 'is',
+            'all' => 'good',
+            'hah' => [123],
+        ]);
 
         $this->assertEquals($expected, $actual);
     }
