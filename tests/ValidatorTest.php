15b41992f64b7df20f153ef1a68dca006600b630
Gwilyn <gwilyn@karmabunny.com.au>
2021-03-09 20:05:46 +1030

    Remove weird varargs hack.

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -27,14 +27,11 @@ class ValidatorTest extends TestCase
     /**
     * @dataProvider dataCheckFailures
     */
-    public function testCheckFailures($value, $func)
+    public function testCheckFailures($value, ...$args)
     {
         $validator = new RulesValidator(['field' => $value]);
 
-        // Awful hack to pass through varargs to the Validator::check method
-        $args = func_get_args();
-        $args[0] = 'field';
-        call_user_func_array([$validator, 'check'], $args);
+        $validator->check('field', ...$args);
 
         $this->assertTrue($validator->hasErrors());
         $this->assertCount(1, $validator->getFieldErrors());
