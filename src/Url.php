e9a593a94f52e23481e530b51b229d81efc4dd7a
Gwilyn <gwilyn@karmabunny.com.au>
2022-07-24 22:54:09 +0930

    Wrap the buitin parse_url() into an object with a set of additional helpers.

--- src/Url.php
+++ src/Url.php
@@ -7,11 +7,263 @@
 namespace karmabunny\kb;
 
 /**
- * URL helper methods.
+ * URL object and helper methods.
+ *
+ * Think of this as an unholy mush of the JS-style URL and URLSearchParams.
  *
  * @package karmabunny\kb
  */
-abstract class Url {
+class Url extends DataObject
+{
+    use UpdateVirtualTrait;
+
+    /** @var string|null */
+    public $scheme;
+
+    /** @var string|null */
+    public $host;
+
+    /** @var string|null */
+    public $port;
+
+    /** @var string|null */
+    public $user;
+
+    /** @var string|null */
+    public $pass;
+
+    /** @var string|null */
+    public $path;
+
+    /** @var array after the question mark ? */
+    public $query = [];
+
+    /** @var string|null after the hashmark # */
+    public $fragment;
+
+
+    /** @inheritdoc */
+    public function virtual(): array
+    {
+        return [
+            'query' => [$this, 'setQuery'],
+        ];
+    }
+
+
+    /**
+     * Replace the query.
+     *
+     * @param array|string $query
+     * @return static
+     * @throws UrlDecodeException
+     */
+    public function setQuery($query)
+    {
+        if (is_array($query)) {
+            $this->query = $query;
+        }
+        else {
+            $this->query = self::decode($query);
+        }
+        return $this;
+    }
+
+
+    /**
+     * Merge parameters into the query.
+     *
+     * @param array $query
+     * @return static
+     * @throws UrlDecodeException
+     */
+    public function addParams(array $query)
+    {
+        $this->query = array_merge($this->query, $query);
+        return $this;
+    }
+
+
+    /**
+     * Set a parameter.
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return static
+     */
+    public function setParam(string $name, $value)
+    {
+        $this->query[$name] = $value;
+        return $this;
+    }
+
+
+    /**
+     * Add to a query parameter.
+     *
+     * This will append to the existing item if it's an array, otherwise
+     * converts it to an array and adds the value.
+     *
+     * @param string $name
+     * @param mixed $value
+     * @return static
+     */
+    public function addParam(string $name, $value)
+    {
+        if (array_key_exists($name, $this->query)) {
+            $existing = $this->query[$name];
+
+            if (is_array($existing)) {
+                $existing[] = $value;
+                $value = $existing;
+            }
+            else {
+                $value = [ $existing, $value ];
+            }
+        }
+
+        $this->query[$name] = $value;
+        return $this;
+    }
+
+
+    /**
+     * Remove a query param by name.
+     *
+     * @param string $name
+     * @return static
+     */
+    public function removeParam(string $name)
+    {
+        unset($this->query[$name]);
+        return $this;
+    }
+
+
+    /**
+     * Does this query param exist?
+     *
+     * @param string $name
+     * @return bool exists
+     */
+    public function hasParam(string $name): bool
+    {
+        return array_key_exists($name, $this->query);
+    }
+
+
+    /**
+     * Remove the port if the scheme infers a default one.
+     *
+     * @return void
+     */
+    public function normalize()
+    {
+        if ($this->scheme) {
+            $default = self::getDefaultPort($this->scheme);
+
+            if ($default == $this->port) {
+                $this->port = null;
+            }
+        }
+    }
+
+
+    /**
+     * Get the query string.
+     *
+     * @return string
+     */
+    public function getQueryString(): string
+    {
+        if (!$this->query) {
+            return '';
+        }
+
+        return self::encode($this->query);
+    }
+
+
+    /**
+     * Build a URL from the parts.
+     *
+     * @return string
+     */
+    public function toString(bool $normalize = true): string
+    {
+        $url = $this;
+
+        if ($normalize) {
+            $url = clone $this;
+            $url->normalize();
+        }
+
+        $out = '';
+
+        if ($url->host) {
+            if ($url->scheme) {
+                $out .= $url->scheme . '://';
+            }
+
+            if ($url->user) {
+                $out .= $url->user;
+
+                if ($url->pass) {
+                    $out .= ':' . $url->pass;
+                }
+
+                $out .= '@';
+            }
+
+            $out .= $url->host;
+
+            if ($url->port) {
+                $out .= ':' . $url->port;
+            }
+        }
+
+        if ($url->path) {
+            if ($out and strpos($url->path, '/') !== 0) {
+                $out .= '/';
+            }
+
+            $out .= $url->path;
+        }
+
+        if ($url->query) {
+            $out .= '?' . $url->getQueryString();
+        }
+
+        if ($url->fragment) {
+            $out .= '#' . $url->fragment;
+        }
+
+        return $out;
+    }
+
+
+    /** @inheritdoc */
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+
+    /**
+     * Parse a string into a URL object.
+     *
+     * @param string $url
+     * @return self
+     */
+    public static function parse(string $url)
+    {
+        $config = parse_url($url);
+        if ($config === false) {
+            throw new UrlParseException("Could not parse URL: {$url}");
+        }
+        return new self($config);
+    }
+
 
     /**
      *
@@ -34,10 +286,33 @@ abstract class Url {
     {
         $result = [];
         if (!mb_parse_str($query, $result)) {
-            throw (new UrlDecodeException('Failed to parse query'))
+            throw (new UrlDecodeException('Failed to decode query'))
                 ->setQuery($query);
         }
         return $result;
+    }
+
+
+    /**
+     *
+     * @return array
+     */
+    public static function getDefaultPorts(): array
+    {
+        static $ports;
+        return $ports ?? include __DIR__ . '/config/ports.php';
+    }
+
+
+    /**
+     *
+     * @param string $scheme
+     * @return null|int
+     */
+    public static function getDefaultPort(string $scheme): ?int
+    {
+        $ports = self::getDefaultPorts();
+        return $ports[$scheme] ?? null;
     }
 
 
@@ -64,6 +339,7 @@ abstract class Url {
     {
         if (empty($parts)) return '/';
 
+        // TODO remove this. I hate it.
         if (is_string($parts[0])) {
             $base = array_shift($parts);
         }
