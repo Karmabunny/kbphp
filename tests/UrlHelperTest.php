405a87a51d90e032895f9b23f3cf42c969bda93a
Gwilyn <gwilyn@karmabunny.com.au>
2022-07-24 22:54:21 +0930

    A bunch of url tests.

--- tests/UrlHelperTest.php
+++ tests/UrlHelperTest.php
@@ -12,6 +12,89 @@ use PHPUnit\Framework\TestCase;
  */
 final class UrlHelperTest extends TestCase {
 
+
+    public function testParse()
+    {
+        $url = 'https://user:pass@example.com:1234/api/path?a=1&b=2#!/fragment=123';
+        $actual = Url::parse($url);
+
+        $this->assertEquals('https', $actual->scheme);
+        $this->assertEquals('example.com', $actual->host);
+        $this->assertEquals(1234, $actual->port);
+        $this->assertEquals('/api/path', $actual->path);
+        $this->assertEquals('user', $actual->user);
+        $this->assertEquals('pass', $actual->pass);
+
+        $expected = [ 'a' => '1', 'b' => '2' ];
+        $this->assertEquals($expected, $actual->query);
+
+        $expected = 'a=1&b=2';
+        $this->assertEquals($expected, $actual->getQueryString());
+
+        $this->assertEquals('!/fragment=123', $actual->fragment);
+
+        $this->assertEquals($url, $actual->toString());
+    }
+
+
+    public function testConstruct()
+    {
+        $actual = new Url([
+            'scheme' => 'http',
+            'host' => 'example.com',
+            'port' => 80,
+            'path' => 'etc/123',
+            'query' => [
+                'a' => '1',
+                'b' => [2,3,4],
+            ],
+        ]);
+
+        $this->assertEquals('http', $actual->scheme);
+        $this->assertEquals('example.com', $actual->host);
+        $this->assertEquals(80, $actual->port);
+        $this->assertEquals('etc/123', $actual->path);
+
+        $expected = [ 'a' => '1', 'b' => [2,3,4] ];
+        $this->assertEquals($expected, $actual->query);
+
+        $expected = 'a=1&b%5B0%5D=2&b%5B1%5D=3&b%5B2%5D=4';
+        $this->assertEquals($expected, $actual->getQueryString());
+
+        $url = 'http://example.com/etc/123?a=1&b%5B0%5D=2&b%5B1%5D=3&b%5B2%5D=4';
+        $this->assertEquals($url, $actual->toString());
+    }
+
+
+    public function testShortConstruct()
+    {
+        $actual = new Url([
+            'host' => 'example.com',
+            'path' => 'etc/123',
+            'query' => [
+                'a' => '1',
+                'b' => [2,3,4],
+            ],
+        ]);
+
+        $this->assertNull($actual->scheme);
+        $this->assertNull($actual->port);
+        $this->assertNull($actual->fragment);
+
+        $this->assertEquals('example.com', $actual->host);
+        $this->assertEquals('etc/123', $actual->path);
+
+        $expected = [ 'a' => '1', 'b' => [2,3,4] ];
+        $this->assertEquals($expected, $actual->query);
+
+        $expected = 'a=1&b%5B0%5D=2&b%5B1%5D=3&b%5B2%5D=4';
+        $this->assertEquals($expected, $actual->getQueryString());
+
+        $url = 'example.com/etc/123?a=1&b%5B0%5D=2&b%5B1%5D=3&b%5B2%5D=4';
+        $this->assertEquals($url, $actual->toString());
+    }
+
+
     public function testWithBaseUrl()
     {
         // Typical usage: the base url isn't cleaned, the path built and _is_ cleaned.
@@ -38,7 +121,7 @@ final class UrlHelperTest extends TestCase {
         // The correct way would be to decode it first.
         // Something like this.
         $expected = 'https://okay.com/it/is?all=good&hah%5B0%5D=123';
-        [$path, $query] = explode('?', '///it/is?all=good&hah[0]=123');
+        [$path, $query] = explode('?', '///it/is?all=good&hah[0]=123', 2);
         $actual = Url::build('https://okay.com', $path, Url::decode($query));
 
         $this->assertEquals($expected, $actual);
