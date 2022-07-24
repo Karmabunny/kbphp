adacd14b68bbeb36f43636b9cfe7e02350ad821a
Gwilyn <gwilyn@karmabunny.com.au>
2022-07-24 22:53:18 +0930

    A new 'url parse' exception.

---
+++ src/UrlParseException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link      https://github.com/Karmabunny
+ * @copyright Copyright (c) 2020 Karmabunny
+ */
+
+namespace karmabunny\kb;
+
+/**
+ * Url parse errors.
+ *
+ * @package karmabunny\kb
+ */
+class UrlParseException extends UrlException
+{
+}
