910406011bba9ac125518132ccb126fda2d25acc
Gwilyn <gwilyn@karmabunny.com.au>
2022-11-04 13:42:40 +1030

    Merge virtual helper into update virtuals trait.

--- src/UpdateVirtualTrait.php
+++ src/UpdateVirtualTrait.php
@@ -6,9 +6,13 @@
 
 namespace karmabunny\kb;
 
+use Closure;
+
 /**
  * This modifies the behaviour of a DataObject/Collection for updating complex
  * properties, such as arrays and objects.
+ *
+ * Implement the {@see UpdateVirtualInterface} to enable this for DataObject types.
  *
  * @package karmabunny\kb
  */
@@ -16,12 +20,21 @@ trait UpdateVirtualTrait
 {
 
     /**
-     * A list of converter functions to apply to the data before saving.
+     * A list of converter functions or target objects.
      *
      * Example:
      * ```php
      * [
-     *     'destinations' => [$this, 'setDestinations'],
+     *     // Convert a single object.
+     *     'user' => User::class,
+     *
+     *     // Convert an array of objects.
+     *     'sites' => [Sites::class],
+     *
+     *     // A local converter method.
+     *     'other' => [$this, 'someMethod'],
+     *
+     *     // Some external method.
      *     'some_string_i_guess' => 'trim',
      * ]
      * ```
@@ -31,6 +44,46 @@ trait UpdateVirtualTrait
     public function virtual(): array
     {
         return [];
+    }
+
+
+    /**
+     * Apply virtual properties.
+     *
+     * @param iterable $config
+     * @return void
+     */
+    public function setVirtual($config)
+    {
+        $virtuals = $this->virtual();
+
+        foreach ($config as $name => $value) {
+            $virtual = $virtuals[$name] ?? null;
+
+            // Build objects if present.
+            // do this first incase it's callable.
+            if (is_object($virtual) and !($virtual instanceof Closure)) {
+                $this->{$name} = Configure::configure([$virtual => $value]);
+                continue;
+            }
+
+            // Mirror behaviour from UpdateVirtualTrait.
+            if (is_callable($virtual)) {
+                $virtual($value);
+                continue;
+            }
+
+            // Nested array behaviour.
+            if (is_array($virtual) and class_exists($virtual[0] ?? '')) {
+                $this->{$name} = [];
+
+                foreach ($value as $key => $item) {
+                    $this->{$name}[$key] = Configure::configure([$virtual[0] => $item]);
+                }
+
+                continue;
+            }
+        }
     }
 
 
