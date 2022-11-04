8427e08f40db2530bb54723ff63334dbdcadcf43
Gwilyn <gwilyn@karmabunny.com.au>
2022-11-04 12:09:01 +1030

    Simplify single/array modes for virtual object attributes.

--- src/VirtualObject.php
+++ src/VirtualObject.php
@@ -23,36 +23,31 @@ use ReflectionProperty;
 class VirtualObject extends VirtualPropertyBase
 {
 
-    const MODE_SINGLE = 1;
-    const MODE_ARRAY = 2;
-    const MODE_PRESERVE_KEYS = 6;
-
-
     /**
      * @var string
      */
     public $class;
 
 
-    /** @var int */
-    public $mode = self::MODE_SINGLE;
+    /** @var bool */
+    public $single = true;
 
 
     /**
      *
      * @param string $method
-     * @param int|bool|string $mode
+     * @param bool $single
      * @return void
      * @throws InvalidArgumentException
      */
-    public function __construct(string $class, $mode = self::MODE_SINGLE)
+    public function __construct(string $class, bool$single = true)
     {
         if (!class_exists($class)) {
             throw new InvalidArgumentException("Target class does not exist: {$class}");
         }
 
         $this->class = $class;
-        $this->mode = self::parseMode($mode);
+        $this->single = $single;
     }
 
 
@@ -65,7 +60,7 @@ class VirtualObject extends VirtualPropertyBase
 
         $this->reflect->setAccessible(true);
 
-        if ($this->mode & self::MODE_SINGLE) {
+        if ($this->single) {
             $value = Configure::configure([ $this->class => $value ]);
             $this->reflect->setValue($target, $value);
         }
@@ -74,49 +69,11 @@ class VirtualObject extends VirtualPropertyBase
 
             foreach ($value as $key => $item) {
                 $item = Configure::configure([ $this->class => $item ]);
-
-                if ($this->mode & self::MODE_PRESERVE_KEYS) {
-                    $items[$key] = $item;
-                } else {
-                    $items[] = $item;
-                }
+                $items[$key] = $item;
             }
 
             $this->reflect->setValue($target, $items);
         }
     }
 
-
-    /**
-     *
-     * @param int|bool|string $mode
-     * @return int
-     */
-    public static function parseMode($mode)
-    {
-        if (is_string($mode)) {
-            $mode = strtolower($mode);
-
-            switch ($mode) {
-                case 'array':
-                    return self::MODE_ARRAY;
-
-                case 'preserve_keys':
-                    return self::MODE_PRESERVE_KEYS;
-
-                case 'single':
-                    return self::MODE_SINGLE;
-
-                // default fallthrough.
-            }
-        }
-        else if (is_bool($mode)) {
-            return $mode ? self::MODE_PRESERVE_KEYS : self::MODE_SINGLE;
-        }
-        else if (is_int($mode)) {
-            return $mode;
-        }
-
-        throw new InvalidArgumentException("Invalid mode: {$mode}");
-    }
 }
