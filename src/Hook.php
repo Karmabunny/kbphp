d7e898273bc98c6591a734bb51e13bd14377d52d
Gwilyn <gwilyn@karmabunny.com.au>
2022-11-04 11:29:37 +1030

    Some docs about hooks.

--- src/Hook.php
+++ src/Hook.php
@@ -11,6 +11,21 @@ use ReflectionMethod;
 use RuntimeException;
 
 /**
+ * Hooks are an iter-communication within objects, particularly for
+ * communication across traits.
+ *
+ * Traits pose a unique problem, by nature they are an optional feature set to
+ * include in a class. But at times these traits have code that needs to run in
+ * certain places to operate correctly. Without rewriting those methods, there
+ * is no clean way to invoke trait methods or detect which traits exists.
+ *
+ * Usage:
+ *
+ * 1. Code will invoke a hook by calling `_hook()` while declaring a hook ID
+ * and any associated arguments.
+ *
+ * 2. A trait will attach to a hooks by using the `#[Hook]` attribute
+ * (in PHP 8+) or the `@hook` tag with the same hook ID.
  *
  * @attribute hook method
  * @package karmabunny\kb
