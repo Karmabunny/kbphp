de9fb3a692f7d7f98eb83c95e2ffca02e8c67ed8
Gwilyn <gwilyn@karmabunny.com.au>
2022-12-07 17:54:39 +1030

    Update phpdoc script.

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 cd "$(dirname $0)/.."
-./scripts/phpdoc.sh \
-    --directory src \
-    --target docs \
+set -e
+
+PHPDOC="./.phpdoc.phar"
+
+# Install phpdoc
+if ! [[ -f $PHPDOC ]]; then
+    wget https://phpdoc.org/phpDocumentor.phar -O $PHPDOC
+
+    if [[ $? != 0 ]]; then
+        rm $PHPDOC
+        exit 1
+    fi
+
+    chmod +x $PHPDOC
+fi
+
+# Build API classes
+$PHPDOC run \
+    --directory src/ \
+    --target docs/ \
     --no-interaction \
     --no-ansi
+
 exit $?
