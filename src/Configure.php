ecef4803e1cc94183202609b20309d1638a8cf94
Gwilyn <gwilyn@karmabunny.com.au>
2023-08-25 17:50:41 +0930

    Move configure implementation to the static class.

--- src/Configure.php
+++ src/Configure.php
@@ -6,6 +6,8 @@
 
 namespace karmabunny\kb;
 
+use InvalidArgumentException;
+
 /**
  * Utility for configuring things.
  *
@@ -13,7 +15,86 @@ namespace karmabunny\kb;
  */
 class Configure
 {
-    use ConfigureTrait;
+
+    /**
+     * Configure an object.
+     *
+     * If 'configurable' this will use the 'update()' method. Otherwise it'll
+     * set properties directly on the object.
+     *
+     * The config is a key-value array, for example:
+     *
+     * ```
+     * [
+     *     '\\My\\Big\\Class' => [
+     *        'foo' => 'bar',
+     *        'baz' => 'qux',
+     *     ]
+     * ]
+     * ```
+     *
+     * Given a string the 'config' is implicitly an empty array.
+     *
+     * @param string|array|object $config [ class => config ]
+     * @param string|null $assert class name to verify
+     * @param bool $init whether to initialize the object after creation
+     * @return object
+     * @throws InvalidArgumentException
+     */
+    public static function configure($config, string $assert = null, bool $init = true)
+    {
+        // Mush it into a key-config pair.
+        if (is_string($config)) {
+            $config = [ $config => [] ];
+        }
+
+        // Pass through objects.
+        if (is_object($config)) {
+            $class = get_class($config);
+        }
+        // It's an array.
+        else {
+            $class = key($config);
+            $config = reset($config);
+        }
+
+        // Check that we're all behaving here.
+        if ($assert
+            and $assert !== $class
+            and !is_subclass_of($class, $assert)
+        ) {
+            throw new InvalidArgumentException("{$class} must extend '{$assert}'");
+        }
+
+        // Pass-through the object.
+        if (is_object($config)) {
+            if ($init and is_subclass_of($config, ConfigurableInit::class)) {
+                $config->init();
+            }
+            return $config;
+        }
+
+        // Do configurable things because we can.
+        else if (is_subclass_of($class, Configurable::class)) {
+            $object = new $class();
+            $object->update($config);
+        }
+        // Or just the regular.
+        else {
+            $object = new $class();
+
+            foreach ($config as $key => $value) {
+                $object->$key = $value;
+            }
+        }
+
+        // Call the init() function, if present.
+        if ($init and is_subclass_of($object, ConfigurableInit::class)) {
+            $object->init();
+        }
+
+        return $object;
+    }
 
 
     /**
--- src/ConfigureTrait.php
+++ src/ConfigureTrait.php
@@ -41,59 +41,10 @@ trait ConfigureTrait
      * @param string|null $assert class name to verify
      * @param bool $init whether to initialize the object after creation
      * @return object
+     * @throws InvalidArgumentException
      */
     public static function configure($config, string $assert = null, bool $init = true)
     {
-        // Mush it into a key-config pair.
-        if (is_string($config)) {
-            $config = [ $config => [] ];
-        }
-
-        // Pass through objects.
-        if (is_object($config)) {
-            $class = get_class($config);
-        }
-        // It's an array.
-        else {
-            $class = key($config);
-            $config = reset($config);
-        }
-
-        // Check that we're all behaving here.
-        if ($assert
-            and $assert !== $class
-            and !is_subclass_of($class, $assert)
-        ) {
-            throw new InvalidArgumentException("{$class} must extend '{$assert}'");
-        }
-
-        // Pass-through the object.
-        if (is_object($config)) {
-            if ($init and is_subclass_of($config, ConfigurableInit::class)) {
-                $config->init();
-            }
-            return $config;
-        }
-
-        // Do configurable things because we can.
-        else if (is_subclass_of($class, Configurable::class)) {
-            $object = new $class();
-            $object->update($config);
-        }
-        // Or just the regular.
-        else {
-            $object = new $class();
-
-            foreach ($config as $key => $value) {
-                $object->$key = $value;
-            }
-        }
-
-        // Call the init() function, if present.
-        if ($init and is_subclass_of($object, ConfigurableInit::class)) {
-            $object->init();
-        }
-
-        return $object;
+        return Configure::configure($config, $assert, $init);
     }
 }
