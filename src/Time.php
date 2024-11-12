c22e2ec16242172c8a0e27501cccacf040ae3108
Gwilyn <gwilyn@karmabunny.com.au>
2024-11-12 14:32:40 +1100

    Add timezone date helpers.

--- src/Time.php
+++ src/Time.php
@@ -323,6 +323,91 @@ class Time
 
 
     /**
+     * Convert a timestamp to a date string in the given timezone
+     *
+     * @param string $timezone
+     * @param int $timestamp
+     * @param string $format
+     * @return string The date string in the requested format
+     */
+    public static function utcTimeToDate(string $timezone, int $timestamp, $format = 'Y-m-d H:i:s')
+    {
+        $timezone_dt = new DateTimeZone($timezone);
+        $date = new DateTime('@'.$timestamp);
+        $date->setTimezone($timezone_dt);
+
+        return $date->format($format);
+    }
+
+
+    /**
+     * Convert a date string to a timestamp in the given timezone
+     *
+     * @param string $timezone
+     * @param string $date
+     * @return int The timestamp
+     */
+    public static function utcDateToTime(string $timezone, string $date)
+    {
+        $timezone_dt = new DateTimeZone($timezone);
+        $date = new DateTime($date, $timezone_dt);
+
+        return $date->getTimestamp();
+    }
+
+
+    /**
+     * Convert a UTC date string to a date string in the given timezone
+     *
+     * @param string $timezone
+     * @param string $date
+     * @param string $format
+     *
+     * @return string The date string in the requested format
+     */
+    public static function utcDateToLocal(string $timezone, string $date, $format = 'Y-m-d H:i:s')
+    {
+        $timezone_dt = new DateTimeZone('UTC');
+        $date = new DateTime($date, $timezone_dt);
+        $date->setTimezone(new DateTimeZone($timezone));
+
+        return $date->format($format);
+    }
+
+
+    /**
+     * Convert a local date string to a date string in UTC
+     *
+     * @param string $timezone
+     * @param string $date
+     * @param string $format
+     *
+     * @return string The date string in the requested format
+     */
+    public static function localDateToUtc(string $timezone, string $date, $format = 'Y-m-d H:i:s')
+    {
+        $timezone_dt = new DateTimeZone($timezone);
+        $date = new DateTime($date, $timezone_dt);
+        $date->setTimezone(new DateTimeZone('UTC'));
+
+        return $date->format($format);
+    }
+
+
+    /**
+     * Get the current datetime in the given timezone
+     *
+     * @param string $timezone
+     * @param string $format
+     * @return string The date string in the requested format
+     */
+    public static function localNow(string $timezone, $format = 'Y-m-d H:i:s')
+    {
+        return self::utcTimeToDate($timezone, time(), $format);
+    }
+
+
+    /**
      * Modify an interval.
      *
      * As such, a contrived example:
