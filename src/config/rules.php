62811001e6a4aeda48ea4013131c3425fa7a6809
Gwilyn <gwilyn@karmabunny.com.au>
2025-01-31 17:05:30 +1100

    Add new validator for rule classes.

---
+++ src/config/rules.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    karmabunny\kb\rules\RequiredRule::class,
+    karmabunny\kb\rules\AllInArrayRule::class,
+    karmabunny\kb\rules\AllMatchRule::class,
+    karmabunny\kb\rules\AllUniqueRule::class,
+    karmabunny\kb\rules\BinaryRule::class,
+    karmabunny\kb\rules\DateRangeRule::class,
+    karmabunny\kb\rules\EmailRule::class,
+    karmabunny\kb\rules\InArrayRule::class,
+    karmabunny\kb\rules\Ipv4AddrOrCidrRule::class,
+    karmabunny\kb\rules\Ipv4AddrRule::class,
+    karmabunny\kb\rules\Ipv4CidrRule::class,
+    karmabunny\kb\rules\LengthRule::class,
+    karmabunny\kb\rules\MysqlDateRule::class,
+    karmabunny\kb\rules\MysqlDateTimeRule::class,
+    karmabunny\kb\rules\MysqlTimeRule::class,
+    karmabunny\kb\rules\NumericRule::class,
+    karmabunny\kb\rules\OneRequiredRule::class,
+    karmabunny\kb\rules\PasswordRule::class,
+    karmabunny\kb\rules\PhoneRule::class,
+    karmabunny\kb\rules\PositiveIntRule::class,
+    karmabunny\kb\rules\ProseTextRule::class,
+    karmabunny\kb\rules\RangeRule::class,
+    karmabunny\kb\rules\RegexRule::class,
+];
