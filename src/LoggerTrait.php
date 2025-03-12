7022b11dd8eb45ac2659be25dae51d0f5f9109dc
Gwilyn <gwilyn@karmabunny.com.au>
2025-03-12 11:18:55 +1100

    Add loggable filtering.

--- src/LoggerTrait.php
+++ src/LoggerTrait.php
@@ -40,12 +40,71 @@ trait LoggerTrait {
     /**
      * Forward any logs from this loggable to a parent loggable.
      *
+     * Logs can be filtered by level and category.
+     *
+     * ```
+     * // Only permit log levels higher than this:
+     * // (excludes INFO, DEBUG, etc)
+     * $loggable->attach($parent, Log::LEVEL_WARNING);
+     *
+     * // Only permit this category:
+     * $loggable->attach($parent, null, 'progress');
+     *
+     * // Only permit these categories:
+     * $loggable->attach($parent, null, ['log.request', 'log.response']);
+     *
+     * // Exclude these categories:
+     * $loggable->attach($parent, null, ['stats' => false, 'meta' => false]);
+     * ```
+     *
      * @param Loggable $parent
+     * @param string|array|null $category filter by category
+     * @param int|null $level filter by level
      * @return void
      */
-    public function attach(Loggable $parent)
+    public function attach(Loggable $parent, ?int $level = null, $category = null)
     {
-        $this->addLogger([$parent, 'log']);
+        $filter = [];
+
+        $category = (array) $category;
+
+        foreach ($category as $key => $item) {
+            $invert = false;
+
+            if (!is_numeric($key)) {
+                $invert = $item === false;
+                $item = $key;
+            }
+
+            if ($invert) {
+                $filter['exclude'][$item] = true;
+            }
+            else {
+                $filter['permit'][$item] = true;
+            }
+        }
+
+        $this->addLogger(function($message, $_level, $_category, $_timestamp) use ($parent, $filter, $level) {
+
+            if ($filter) {
+                if (isset($filter['exclude'][$_category])) {
+                    return;
+                }
+
+                if (
+                    !empty($filter['permit'])
+                    and !isset($filter['permit'][$_category])
+                ) {
+                    return;
+                }
+            }
+
+            if ($level and $level < $_level) {
+                return;
+            }
+
+            $parent->log($message, $_level, $_category, $_timestamp);
+        });
     }
 
 
