0d5baf9f71d5c2db4ce39303b608a3c40465483b
Gwilyn <gwilyn@karmabunny.com.au>
2026-09-07 17:04:24 +1000

    Add cast date helper.

---
+++ src/CastDate.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @link      https://github.com/Karmabunny
+ * @copyright Copyright (c) 2026 Karmabunny
+ */
+
+namespace karmabunny\kb;
+
+use Attribute;
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
+use DateTimeZone;
+use InvalidArgumentException;
+use ReflectionProperty;
+use Throwable;
+
+/**
+ * Cast dates from valid sources.
+ *
+ * Timezone behaviours:
+ *
+ * - 'default' retains the TZ of a given value and only applies the
+ *   system TZ if one is absent (strings, numbers).
+ *
+ * - 'system' sets the TZ to the system TZ, always.
+ *
+ * - 'utc' sets the TZ to UTC, always.
+ *
+ * - <iso-name> sets the TZ to the given ISO name, e.g. 'Europe/London'.
+ *
+ * @package karmabunny\kb
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class CastDate extends Cast
+{
+
+    /**
+     *
+     * @param string $timezone default|system|utc|<iso-name>
+     * @return void
+     */
+    public function __construct(public string $timezone = 'default')
+    {
+    }
+
+
+    /** @inheritdoc */
+    public function build(mixed $value): mixed
+    {
+        $property = new ReflectionProperty($this->target, $this->property);
+        $type = $property->getType();
+
+        $zone = match ($this->timezone) {
+            'default' => null,
+            'system' => new DateTimeZone(date_default_timezone_get()),
+            'utc' => new DateTimeZone('UTC'),
+            default => new DateTimeZone($this->timezone),
+        };
+
+        $class = $type->getName();
+
+        if (!is_subclass_of($class, DateTimeInterface::class)) {
+            throw new InvalidArgumentException("Invalid date type: {$class}");
+        }
+
+        /** @var class-string<DateTime|DateTimeImmutable> $class */
+
+        try {
+            // Pass-through.
+            if ($value instanceof DateTimeInterface) {
+                $value = $class::createFromInterface($value);
+            }
+            // Parse integer/floats as timestamps with microseconds.
+            else if (is_numeric($value)) {
+                $value = $class::createFromFormat('U.u', sprintf('%.6f', $value), $zone);
+            }
+            // Classic timey-wimey parsing.
+            else if (is_string($value)) {
+                $value = new $class($value, $zone);
+            }
+
+            return $value;
+        }
+        catch (Throwable $error) {
+            if ($this->isNullable()) {
+                return null;
+            }
+
+            throw $error;
+        }
+    }
+}
